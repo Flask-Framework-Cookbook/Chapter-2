fca1881ed4c76f8d48a860fbd87fdb144f66c6b3
Shalabh Aggarwal <shalabh7777@gmail.com>
2014-09-21 15:35:44 +0530

    Chapter 2 - Recipe 'Block Composition and Layout inheritance'

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from my_app.hello.views import hello
+from my_app.product.views import product_blueprint
 
 app = Flask(__name__)
-app.register_blueprint(hello)
+app.register_blueprint(product_blueprint)
