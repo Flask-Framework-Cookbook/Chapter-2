d3021612a97e16434d642b76dc0e27cb610ecedc
Shalabh Aggarwal <shalabh7777@gmail.com>
2019-02-07 15:29:21 +0530

    Add a failsafe in case accept_languages does not detect a language

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -8,5 +8,5 @@ app.register_blueprint(product_blueprint)
 
 @app.template_filter('format_currency')
 def format_currency_filter(amount):
-    currency_code = ccy.countryccy(request.accept_languages.best[-2:])
-    return '{0} {1}'.format(currency_code, amount)
+	currency_code = ccy.countryccy(request.accept_languages.best[-2:]) or 'USD'
+	return '{0} {1}'.format(currency_code, amount)
