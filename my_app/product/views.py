f022f07bed3a33113a8b3dce06a8fa3d2a80f9f1
Shalabh Aggarwal <shalabh7777@gmail.com>
2020-01-13 14:49:14 +0700

    [Anti-Pattern correct] Fix redefining of names from outer scope

--- my_app/product/views.py
+++ my_app/product/views.py
@@ -8,8 +8,8 @@ product_blueprint = Blueprint('product', __name__)
 
 @product_blueprint.context_processor
 def some_processor():
-    def full_name(product):
-        return '{0} / {1}'.format(product['category'], product['name'])
+    def full_name(prod):
+        return '{0} / {1}'.format(prod['category'], prod['name'])
     return {'full_name': full_name}
 
 
@@ -20,7 +20,7 @@ def home():
 
 @product_blueprint.route('/product/<key>')
 def product(key):
-    product = PRODUCTS.get(key)
-    if not product:
+    prod = PRODUCTS.get(key)
+    if not prod:
         abort(404)
-    return render_template('product.html', product=product)
+    return render_template('product.html', product=prod)
